018d0288375d07eb286eb4c2f3e8869863e4a43e
NolanKnight <nolan.r.knight@gmail.com>
2023-03-18 21:06:39 -0700

    setup framework

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 import { sendToVercelAnalytics } from './vitals';
+import Home from './pages/home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/pages/home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <>
+      <h1 className="text-3xl">Home</h1>
+      <p>Welcome!</p>
+    </>
+  );
+}
+
+export default Home;
